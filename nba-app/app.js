86a5e746d109d4e891d7d1dd926f192119d79316
Rémy Lenoir <remylenoir.dev@tuta.io>
2019-04-12 13:08:27 +0200

    NBA update

--- nba-app/app.js
+++ nba-app/app.js
@@ -1,36 +1,42 @@
-const fs = require("fs");
-const express = require("express");
-const hbs = require("hbs");
+const fs = require('fs');
+const express = require('express');
+const hbs = require('hbs');
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.set("views", __dirname + "/views");
-app.set("view engine", "hbs");
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
 
-hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerPartials(__dirname + '/views/partials');
 
-const playersList = JSON.parse(fs.readFileSync("players.json", "utf-8"));
+const playersList = JSON.parse(fs.readFileSync('players.json', 'utf-8'));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.get("/players/:playerName", (req, res) => {
+app.get('/players/:playerName', (req, res) => {
   const { playerName } = req.params;
+
   // find in the playersList the object for who the name equals the playerName in the params
-  const player = playersList.find(player => player.name.toLowerCase() === playerName.toLowerCase());
-  res.render("player", player);
+
+  const player = playersList.find(
+    el => el.name.toLowerCase() === playerName.toLowerCase()
+  );
+
+  if (!player) res.send('Player not found!');
+  else res.render('player', player);
 });
 
-app.get("/players", (req, res) => {
-  res.render("players", {
+app.get('/players', (req, res) => {
+  res.render('players', {
     playersList: playersList
   });
 });
 
-app.get("/teams", (req, res) => {
-  res.render("teams", {
+app.get('/teams', (req, res) => {
+  res.render('teams', {
     layout: false
   });
 });
